28d353427570c0fdd8c0a9a9916d641c336d7019
Tomas Nilsson <contact@tomas.nu>
2021-10-17 16:54:21 +0200

    Updates

--- build/_/AV/mp4/2015-10-12 Split mp4 without reencoding.md
+++
@@ -1,8 +0,0 @@
-# Split mp4 without reencoding
-
-* -t = duration
-* -ss = position (input/output)
-
-```bash
-ffmpeg -i source.mp4 -t 00:00:50 -c copy small-1.mp4 -ss 00:00:50 -codec copy small-2.mp4
-```
--- build/_/Development/Jira/debug.groovy
+++
@@ -1,7 +0,0 @@
-// *************************************************
-// * For debugging
-// *************************************************
-
-def mySubTask = 'MYTASK-123'
-def issueResp = get("/rest/api/2/issue/${mySubTask}").asObject(Map)
-def issue = issueResp.body as Map
